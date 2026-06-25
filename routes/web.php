c1560ba8eaea061af3099587c121889d596555b3
Nurul-Ilmi <60200124036@uin-alauddin.ac.id>
2026-06-25 20:37:50 +0800

    Setup: Database, Model, and Routes structure

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Rute milik Anggota 2 (Melihat Task)
+Route::get('/tasks', [TaskController::class, 'index']);
+
+// Rute milik Anggota 1 / Kamu (Membuat Task)
+Route::post('/tasks', [TaskController::class, 'store']);
+
+// Rute milik Anggota 2 (Menghapus Task)
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
