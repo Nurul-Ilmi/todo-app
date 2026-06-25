29fc130e1692b647795d4942037b29500a2f2383
Nurul-Ilmi <60200124036@uin-alauddin.ac.id>
2026-06-25 20:45:11 +0800

    Feat: implement create task function

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255'
+        ]);
+
+        $task = Task::create([
+            'title' => $request->title
+        ]);
+
+        return response()->json([
+            'message' => 'Task berhasil ditambahkan!',
+            'data' => $task
+        ], 201);
+    }
 }
